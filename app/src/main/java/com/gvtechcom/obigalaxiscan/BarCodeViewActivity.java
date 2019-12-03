5209e254a095ea6238293245d12d30617fd16cd7
hoangandhv <nguyenvanan808@gmail.com>
2019-12-03 22:10:12 +0700

    Cmit API >21

---
+++ app/src/main/java/com/gvtechcom/obigalaxiscan/BarCodeViewActivity.java
@@ -0,0 +1,238 @@
+package com.gvtechcom.obigalaxiscan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.ResultPoint;
+import com.google.zxing.client.android.BeepManager;
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+import com.journeyapps.barcodescanner.DefaultDecoderFactory;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class BarCodeViewActivity extends AppCompatActivity {
+    private DecoratedBarcodeView barcodeView;
+    private BeepManager beepManager;
+    private String lastText;
+    Dialog dialog;
+    private static String fileNametTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bar_code_view);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+        barcodeView = (DecoratedBarcodeView) findViewById(R.id.barcode_scanner);
+
+        callScan();
+
+    }
+    private void callScan(){
+        Collection<BarcodeFormat> formats = Arrays.asList(BarcodeFormat.QR_CODE, BarcodeFormat.CODE_39);
+        barcodeView.getBarcodeView().setDecoderFactory(new DefaultDecoderFactory(formats));
+        barcodeView.initializeFromIntent(getIntent());
+        barcodeView.decodeContinuous(callback);
+        beepManager = new BeepManager(this);
+    }
+
+    private BarcodeCallback callback = new BarcodeCallback() {
+        @Override
+        public void barcodeResult(BarcodeResult result) {
+            if(result.getText() == null || result.getText().equals(lastText)) {
+                // Prevent duplicate scans
+                return;
+            }
+
+            lastText = result.getText();
+            barcodeView.setStatusText("Đang gửi lên hệ thống");
+
+            beepManager.playBeepSoundAndVibrate();
+
+            //Added preview of scanned barcode
+//            ImageView imageView = (ImageView) findViewById(R.id.barcodePreview);
+//            imageView.setImageBitmap(result.getBitmapWithResultPoints(Color.YELLOW));
+            Bitmap bitmap = result.getBitmapWithResultPoints(Color.YELLOW);
+            File newFile = null;
+            try {
+                newFile = savebitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Uri uri= Uri.fromFile(newFile);
+            showDialogImage(uri);
+            String fileName = fileNametTime;
+            MainActivity.Companion.setNAME_IMAGE(lastText);
+            POST(MainActivity.Companion.getUrl(), newFile, fileName, new Callback() {
+                @Override
+                public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            barcodeView.setStatusText("Xem kết quả trên màn hình!");
+                            Toast.makeText(getBaseContext(), "Gửi thành công!", Toast.LENGTH_SHORT).show();
+                            Handler handles =new Handler();
+                            handles.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    finish();
+                                    MainActivity.Companion.setChekScanQR(true);
+                                    dialog.dismiss();
+                                }
+                            },5000);
+                        }
+                    });
+
+
+                }
+                @Override
+                public void onFailure(@NotNull Call call, @NotNull final IOException e) {
+                    runOnUiThread(new Runnable(){
+
+                        @Override
+                        public void run() {
+                            barcodeView.setStatusText("Không kết nối được!");
+                            Toast.makeText(getBaseContext(),"Lỗi: "+e,Toast.LENGTH_LONG).show();
+                            dialog.dismiss();
+                            Handler handles =new Handler();
+                            handles.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    finish();
+                                    MainActivity.Companion.setChekScanQR(true);
+                                }
+                            },1000);
+                        }
+                    });
+
+                }
+
+
+            });
+
+
+        }
+
+        @Override
+        public void possibleResultPoints(List<ResultPoint> resultPoints) {
+        }
+    };
+
+    public static File savebitmap(Bitmap bmp) throws IOException {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        fileNametTime = "Obi_"+System.currentTimeMillis()+".jpg";
+        File f = new File(Environment.getExternalStorageDirectory()
+                + File.separator + fileNametTime);
+        f.createNewFile();
+        FileOutputStream fo = new FileOutputStream(f);
+        fo.write(bytes.toByteArray());
+        fo.close();
+        return f;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        callScan();
+        barcodeView.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        barcodeView.pause();
+    }
+
+    public void pause(View view) {
+        barcodeView.pause();
+    }
+
+    public void resume(View view) {
+        barcodeView.resume();
+    }
+
+    public void triggerScan(View view) {
+        barcodeView.decodeSingle(callback);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return barcodeView.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event);
+    }
+    public Call POST(String url, File file, String fileName, Callback callback){
+        Toast.makeText(this, "Gửi lên hệ thống", Toast.LENGTH_LONG).show();
+        final MediaType MEDIA_TYPE_JPG = MediaType.parse("image/jpg");
+        RequestBody formBody =new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("content",
+                        MainActivity.Companion.getNAME_IMAGE()
+                )
+                .addFormDataPart("image", fileName, RequestBody.create (MEDIA_TYPE_JPG, file))
+                .build();
+
+        Request request =new Request.Builder()
+                .url(url)
+                .post(formBody)
+                .build();
+
+        OkHttpClient client = new OkHttpClient();
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+        return call;
+    }
+
+    private void showDialogImage(Uri uri) {
+        int width = (int)(getResources().getDisplayMetrics().widthPixels*0.70);
+        int height = (int)(getResources().getDisplayMetrics().heightPixels*0.70);
+        dialog =new Dialog(this);
+        View dialogLayout = getLayoutInflater().inflate(R.layout.dialog_image_scan, null);
+        ImageView imageView = dialogLayout.findViewById(R.id.img_View);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(dialogLayout);
+        dialog.getWindow().setLayout(width,height);
+        dialog.getWindow().setBackgroundDrawableResource(R.color.zxing_transparent);
+        imageView.setImageURI(uri);
+        dialog.create();
+        dialog.show();
+
+    }
+}
--- app/src/main/java/com/gvtechcom/obigalaxiscan/MainActivity.kt
+++ app/src/main/java/com/gvtechcom/obigalaxiscan/MainActivity.kt
@@ -1,18 +1,27 @@
 package com.gvtechcom.obigalaxiscan
 
+import android.Manifest
+import android.app.Dialog
 import android.content.Intent
-import android.os.*
-import android.view.*
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
 import android.widget.Toast
-import com.google.zxing.integration.android.IntentIntegrator
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import okhttp3.OkHttpClient
 
 
 class MainActivity : AppCompatActivity(){
 
 
     private var resultFalse = 0 // đang quét QRcode mà tắt app thì =1
-
+    private var VERSION = 0
+    private val MY_CAMERA_REQUEST_CODE = 100
+    private val MY_WRITE_EXTERNAL_STORAGE = 200
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,47 +30,76 @@ class MainActivity : AppCompatActivity(){
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
-        startScanQrcode()
+            openScanQrCode()
 
     }
-    override fun onResume() {
-        if (chekScanQR){
-//            val intent = Intent(this,TakePhotoSurefaceviewActivity::class.java)
-//            startActivity(intent)
-            startScanQrcode()
-            chekScanQR = false
+    private fun checkPermission(): Boolean{
+        when (PackageManager.PERMISSION_DENIED) {
+            ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) -> {
+                ActivityCompat.requestPermissions(this,arrayOf(Manifest.permission.CAMERA),MY_CAMERA_REQUEST_CODE)
+                return false
+            }
+            ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) -> {
+                ActivityCompat.requestPermissions(this,arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),MY_WRITE_EXTERNAL_STORAGE)
+                return false
+            }
+            else -> return true
         }
-        super.onResume()
     }
+    private fun openScanQrCode(){
+        if (checkPermission()){
+            val intent = Intent(this,BarCodeViewActivity::class.java)
+            startActivityForResult(intent,1)
+        }
 
-    private fun startScanQrcode() {
-        var integrator = IntentIntegrator(this)
-        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
-        integrator.setPrompt("Scan QR CODE")
-        integrator.setCameraId(0)
-        integrator.setOrientationLocked(false)
-        integrator.setBeepEnabled(false)
-        integrator.initiateScan()
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        val intentResult = IntentIntegrator.parseActivityResult(requestCode,resultCode,data)
         super.onActivityResult(requestCode, resultCode, data)
         //NAME_IMAGE = data!!.getStringExtra("valueScan")
-        if (intentResult.contents!=null){
+        if (requestCode==1){
             resultFalse = 0
-            NAME_IMAGE = intentResult.contents
-            Toast.makeText(this, NAME_IMAGE+". Vui lòng đợi",Toast.LENGTH_LONG).show()
+           // NAME_IMAGE = data!!.getStringExtra("resultNAMEIMAGE")
+
             val handler = Handler()
             handler.postDelayed({
-                val intent = Intent(this,TakePhotoSurefaceviewActivity::class.java)
-                startActivity(intent)
-            },100)
+                openScanQrCode()
+            },1000)
         } else resultFalse =1
 
     }
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            MY_CAMERA_REQUEST_CODE -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    openScanQrCode()
+                } else {
+                    finish()
+                }
+                return
+            }
+            MY_WRITE_EXTERNAL_STORAGE -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    openScanQrCode()
+                } else {
+                    finish()
+                }
+                return
+            }
+            // Add other 'when' lines to check for other
+            // permissions this app might request.
+            else -> {
+                // Ignore all other requests.
+            }
+        }
+    }
+
     companion object{
         var NAME_IMAGE: String = ""
         var chekScanQR = true
+        val url = "http://10.10.10.43/api/upload"
     }
 
 
